9a484a7b5fc24e874087efb0b68fa8166df074f6
gabrieldominicoxibillo <115641762+gabrieldominicoxibillo@users.noreply.github.com>
2025-10-08 03:54:37 +0800

    Initial commit

---
+++ src/client/java/com/more_pumpkin_faces/MorePumpkinFacesHappyPackClient.java
@@ -0,0 +1,10 @@
+package com.more_pumpkin_faces;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MorePumpkinFacesHappyPackClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
